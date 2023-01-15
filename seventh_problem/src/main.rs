399c0466a21061fc15cc124ba2314c7cb9255fbf
BuildTools <mariothrowsfieball@gmail.com>
2023-01-15 13:28:16 +0100

    function definitions added below main

--- seventh_problem/src/main.rs
+++ seventh_problem/src/main.rs
@@ -45,6 +45,37 @@ impl Node {
     }
 }
 
+fn main() {
+    let input = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
+    let result = solve(input);
+    let sum = sum_points_for_nodes(&result);
+    let print = result.borrow().print_structure(0);
+    println!("{print}");
+    println!("{sum}");
+}
+
 fn solve(input: &str) -> Rc<RefCell<Node>> {
     let root = Rc::new(RefCell::new(Node::default()));
     let lines: Vec<&str> = input.split('\n').collect();
@@ -136,38 +167,6 @@ fn sum_points_for_nodes(tree: &Rc<RefCell<Node>>) -> i32 {
     }
     result
 }
-
-fn main() {
-    let input = "$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k";
-    let result = solve(input);
-    let sum = sum_points_for_nodes(&result);
-    let print = result.borrow().print_structure(0);
-    println!("{print}");
-    println!("{sum}");
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
