585f6398e5d757850b13a229a535efcc3dde6d97
BuildTools <mariothrowsfieball@gmail.com>
2023-01-24 10:32:13 +0100

    solved part two

--- tenth_problem/src/main.rs
+++ tenth_problem/src/main.rs
@@ -8,8 +8,10 @@ enum Instruction {
 fn main() {
     let register = generate_register(variable::INPUT);
     let signals_of_intrest = vec![20, 60, 100, 140, 180, 220];
-    let solution = find_solution(register, signals_of_intrest);
+    let solution = find_solution(&register, signals_of_intrest);
+    let solution_2 = draw_screen(&register);
     println!("The solution is {:?}", solution);
+    println!("{}", solution_2);
 }
 
 fn parse_input(input: &str) -> Vec<Instruction> {
@@ -58,10 +60,29 @@ fn generate_register(input: &str) -> Vec<i32> {
     register
 }
 
-fn find_solution(register: Vec<i32>, intresting_signals: Vec<i32>) -> i32 {
+fn find_solution(register: &Vec<i32>, intresting_signals: Vec<i32>) -> i32 {
     let mut result = 0;
     for intresting_signal in intresting_signals {
         result += register[(intresting_signal - 1) as usize] * intresting_signal
+    }
+    result
+}
+
+fn draw_screen(register: &Vec<i32>) -> String {
+    let mut result = String::from("");
+    for (i, sprite) in register.iter().enumerate() {
+        let index = i % 40;
+        if (index as i32) - sprite < 2 && (index as i32) - sprite > -2 {
+            result += "#"
+        } else {
+            result += "."
+        }
+        if (i + 1) / 40 == 6 {
+            break;
+        }
+        if (index + 1) % 40 == 0 {
+            result += "\n";
+        }
     }
     result
 }
@@ -87,5 +108,18 @@ addx -5";
         let vector = generate_register(input);
         let expceted = -1;
         assert_eq!(expceted, vector[4]);
+    }
+    #[test]
+    fn print() {
+        let input = variable::TEST_DATA;
+        let vector = generate_register(input);
+        let result = draw_screen(vector);
+        let expected = "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....";
+        assert_eq!(expected, result);
     }
 }
